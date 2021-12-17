aa0ce7a8233c45a7d70ac76e2ec57ef940856579
Gamza <flsbdpt194@naver.com>
2021-12-17 14:52:00 +0900

    add permision
    menifest에 퍼미션 추가

--- 2022_Planner_Project/app/src/main/java/com/example/plannerproject/Main3PageList.java
+++ 2022_Planner_Project/app/src/main/java/com/example/plannerproject/Main3PageList.java
@@ -10,5 +10,7 @@ public class Main3PageList extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main3_page_list);
+
+
     }
 }
