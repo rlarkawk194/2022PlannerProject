6dfc7a22246fdcbbb727a901188fb8bd87fd9858
Gamza <flsbdpt194@naver.com>
2021-12-18 17:15:13 +0900

    02 사이트 연동
    -add array xml
    -add 스피너 및 버튼
    스피너로 웹사이트 선택 시 원하는 웹사이트로 이동하게 하는 기능 구현

--- 2022_Planner_Project/app/src/main/java/com/example/plannerproject/Main3PageList.java
+++ 2022_Planner_Project/app/src/main/java/com/example/plannerproject/Main3PageList.java
@@ -9,6 +9,7 @@ import android.webkit.WebChromeClient;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import android.widget.AdapterView;
 
 import com.example.plannerproject.databinding.ActivityMain3PageListBinding;
 import com.example.plannerproject.databinding.ActivityMainBinding;
@@ -16,6 +17,7 @@ import com.example.plannerproject.databinding.ActivityMainBinding;
 public class Main3PageList extends AppCompatActivity {
 
     private ActivityMain3PageListBinding pageListBinding;
+    private String webUrl;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,14 +26,44 @@ public class Main3PageList extends AppCompatActivity {
         View view = pageListBinding.getRoot();
         setContentView(view);
 
+        //스피너 선택시 해당하는 웹사이트 URL 주소를 webUrl에 할당
+        pageListBinding.spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                if (position == 0){
+                    webUrl = getResources().getString(R.string.smOfficial);
+                } else if(position == 1){
+                    webUrl = getResources().getString(R.string.snowboard);
+                } else if(position == 2){
+                    webUrl = getResources().getString(R.string.snowe);
+                }else if(position == 3){
+                    webUrl = getResources().getString(R.string.wiseSm);
+                }else if(position == 4){
+                    webUrl = getResources().getString(R.string.snoway);
+                }
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
 
         WebSettings ws = pageListBinding.webView1.getSettings();
         ws.setJavaScriptEnabled(true); // 자바스크립트 사용 허가
 
-        pageListBinding.webView1.loadUrl("https://www.naver.com/");
-        pageListBinding.webView1.setWebViewClient(new WebViewClient());//웹뷰에 크롬 사용 허용//이 부분이 없으면 크롬에서 alert가 뜨지 않음
-        pageListBinding.webView1.setWebViewClient(new WebViewClientClass());//새창열기 없이 웹뷰 내에서 다시 열기//페이지 이동 원활히 하기위해 사용
+        pageListBinding.btnPgLoad.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                pageListBinding.webView1.loadUrl(webUrl);
+                pageListBinding.webView1.setWebViewClient(new WebViewClient());//웹뷰에 크롬 사용 허용//이 부분이 없으면 크롬에서 alert가 뜨지 않음
+                pageListBinding.webView1.setWebViewClient(new WebViewClientClass());//새창열기 없이 웹뷰 내에서 다시 열기//페이지 이동 원활히 하기위해 사용
+            }
+        });
+
     }
+
+    //페이지 로드 처리를 위한 클래스
     private class WebViewClientClass extends WebViewClient {//페이지 이동
         @Override
         public boolean shouldOverrideUrlLoading(WebView view, String url) {
@@ -40,9 +72,5 @@ public class Main3PageList extends AppCompatActivity {
             return true;
         }
     }
-
-
-
-
 
 }
