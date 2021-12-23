3f59922cafab7f4e0aa2e701f4440ed0a1e07937
Gamza <flsbdpt194@naver.com>
2021-12-23 16:01:49 +0900

    액티비티 추가들

---
+++ 2022_Planner_Project/app/src/main/java/com/example/plannerproject/Main1Planner.java
@@ -0,0 +1,14 @@
+package com.example.plannerproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Main1Planner extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main1_planner);
+    }
+}
